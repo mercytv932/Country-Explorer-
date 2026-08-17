d1fbf4bfe8973c341f32bef5285b2d6930e4f2b0
Yosef <mercytv932@gmail.com>
2026-08-17 18:28:30 -0400

    card creation

--- script.js
+++ script.js
@@ -2,16 +2,33 @@ const input = document.querySelector(".input");
 const filters = document.querySelector(".filters");
 const countryCards = document.querySelector(".country-cards");
 
-const API_KEY = "key";
 const BASE_URL = "https://api.restcountries.com/countries/v5";
 
 fetch(BASE_URL, { headers: { Authorization: `Bearer ${API_KEY}` } })
   .then((response) => response.json())
   .then((data) => {
-    data.forEach((country) => {
+    data.data.objects.forEach((countr) => {
       const countryCard = document.createElement("div");
       countryCard.className = "country-card";
+      const img = document.createElement("img");
 
-      console.log(country);
+      const info = document.createElement("div");
+      const countryName = document.createElement("h3");
+      const population = document.createElement("p");
+      const region = document.createElement("p");
+      const capital = document.createElement("p");
+
+      info.appendChild(countryName);
+      info.appendChild(population);
+      info.appendChild(region);
+      info.appendChild(capital);
+
+      countryCard.appendChild(img);
+      countryCard.appendChild(info);
+
+      console.log(countr.names.common);
+      console.log(countr.population);
+      console.log(countr.region);
     });
+    console.log(data);
   });
