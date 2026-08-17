102cb8741766027f23aa2985e4be16de71e7a8fe
Yosef <mercytv932@gmail.com>
2026-08-17 17:24:15 -0400

    API and URL fetch

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const input = document.querySelector(".input");
 const filters = document.querySelector(".filters");
 const countryCards = document.querySelector(".country-cards");
+
+const API_KEY = "key";
+const BASE_URL = "https://api.restcountries.com/countries/v5";
+
+fetch(BASE_URL, { headers: { Authorization: `Bearer ${API_KEY}` } })
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((country) => {
+      const countryCard = document.createElement("div");
+      countryCard.className = "country-card";
+
+      console.log(country);
+    });
+  });
