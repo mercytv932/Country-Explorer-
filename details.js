510de7bd1282c363f6759d47f2e941c8b5069a25
Yosef <mercytv932@gmail.com>
2026-08-18 14:17:06 -0400

    done with 2nd page details

--- details.js
+++ details.js
@@ -5,7 +5,6 @@ const countryRegion = document.querySelector(".country-region");
 const countrySubregion = document.querySelector(".country-subregion");
 const countryCapital = document.querySelector(".country-capital");
 const countryCurrency = document.querySelector(".country-currency");
-const countryLanguage = document.querySelector(".country-language");
 
 const API_KEY = "rc_live_b009c411a6c94840b5a6e4df783aa746";
 
@@ -32,5 +31,13 @@ function fetchCountryData(countryName) {
       });
 
       console.log(selectedCountry);
+
+      countryNameElement.textContent = selectedCountry.names.common;
+      countryImg.src = selectedCountry.flag.url_png;
+      countryPopulation.textContent = `Population: ${selectedCountry.population.toLocaleString()}`;
+      countryRegion.textContent = `Region: ${selectedCountry.region}`;
+      countrySubregion.textContent = `Subregion: ${selectedCountry.subregion}`;
+      countryCapital.textContent = `Capital: ${selectedCountry.capitals[0].name}`;
+      countryCurrency.textContent = `Currency: ${selectedCountry.currencies[0].name}`;
     });
 }
